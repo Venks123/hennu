2b7433cacc9fec13eeeb9daf9b7c44ba1929457d
Venky <chari.venkatesh@gmail.com>
2014-12-16 22:46:38 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hydril::Application.config.secret_key_base = '946cce57c0277df06d0dfc1178a56202c59ae67131e5e8ab7efbc800a98ec22414bb50f605b1658c02d24ab0ebef25bab992ac20266ebf8c7496675d684c5c28'
